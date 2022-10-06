6c938be1bf9c2d93e5f9e306f8c2094a811b3198
JohnCesnaDev <114229179+JohnCesnaDev@users.noreply.github.com>
2022-10-07 00:43:11 +0200

    1 file modified

--- src/pages/DebiTube.vue
+++ src/pages/DebiTube.vue
@@ -1,14 +1,20 @@
 <template>
   <q-page class="flex flex-center">
-  <div class="row items-start">
-    <div class="col column q-gutter-sm">
-      <q-input outlined v-model="text"></q-input>
-      <q-btn @click="add" color="primary" label="Primary" />
-      <q-btn color="primary" label="Primary" />
-      <q-btn color="primary" label="Primary" />
-    </div> 
+  <div class="row items-start q-gutter-md">
+    <q-card class="shadow-10 q-card--bordered" style="height:800px">
+    <div style="margin:10px">
+      <div class="col column q-gutter-sm" >
+        <q-input outlined v-model="text" @keydown="onKey"></q-input>
+        <q-btn @click="add" color="primary" label="Primary" />
+        <q-btn color="primary" label="Primary" />
+        <q-btn color="primary" label="Primary" />
+      </div>
+    </div>
+    </q-card>
     <div class="col">
-     <q-scroll-area style="height: 800px;width: 400px;">
+    <q-card class="shadow-10 q-card--bordered" style="height:800px">
+    <div style="margin:10px">
+     <q-scroll-area style="height: 780px;width: 400px;">
         <q-tree class="col-12 col-sm-6"
           :nodes="simple"
           node-key="label"
@@ -17,7 +23,9 @@
           v-model:ticked="ticked"
         >
         </q-tree>
-      </q-scroll-area>     
+      </q-scroll-area> 
+      </div>
+      </q-card>   
     </div>
   </div>
   </q-page>
@@ -30,14 +38,7 @@ const selected = ref('Pleasant surroundings');
 const ticked = ref([ 'Quality ingredients', 'Good table presentation' ]);
 
 
-const simple= ref(
-  [
-    {
-      label: 'Satisfied customers',
-      children: [{label:'dfzejqf'},{label:'dfzerqjf'},{label:'dfjrzeqf'},{label:'dfjzgeqf'}],
-    }
-  ]
-);
+const simple= ref([]);
 
 const text=ref("ok");
 const count = ref(0);
@@ -50,6 +51,14 @@ function add() {
   simple.value.push({label: text.value});
   text.value="";
 }
+
+ function onKey (evt) {
+        if(evt.keyCode === 13)
+        {
+        simple.value.push({label: text.value});
+        text.value="";
+        }
+ }
 
 
 </script>
